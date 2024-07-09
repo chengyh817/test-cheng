2f71373538ffafafc2507a532c3656803f8f67d7
chengyh817 <49397065+chengyh817@users.noreply.github.com>
2024-07-09 13:57:20 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "alicloud_vpc" "tfalicloud" {
   cidr_block = var.vpc_cidr
-  name = "terraform-github-action"
+  vpc_name = "terraform-github-action"
   tags = {
     name = "terraform-github-action"
   }
